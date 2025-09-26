bacba679ab82f3a859219ff813bf6342eff0b9a7
SaulHAC <saulalamillah@gmail.com>
2025-09-26 13:02:54 -0500

    fix add cors for normal http requests

--- chat.js
+++ chat.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const cors = require("cors");
 const { PORT } = require("./config.js");
 
 const app = express();
+
+// CORS para requests http normales
+app.use(cors({ origin: "*" }));
+
 const server = http.createServer(app);
+
+// CORS para socket.io
 const io = new Server(server, {
   cors: {
     origin: "*",
+    methods: ["GET", "POST"],
   },
 });
 
