94c0125196b05080fd9619556a72fb404e63b557
SaulHAC <saulalamillah@gmail.com>
2025-09-26 12:48:56 -0500

    first commit

---
+++ chat.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+import { PORT } from "./config.js";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Un usuario se conectó:", socket.id);
+
+  socket.on("chat-message", (msg) => {
+    io.emit("chat-message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Un usuario se desconectó");
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
